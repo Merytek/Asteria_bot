1ee831d2ea4b30cc94155ee2022ab0412167b860
Aether <asp3kt47@gmail.com>
2020-12-16 17:52:29 +0100

    Ajout des levels auto
    ajout du programme permettant d'ajouter les rôles en fonction du niveau de l'utilisateur

--- event/client/message.js
+++ event/client/message.js
@@ -43,6 +43,66 @@ module.exports = async (Root, message) => {
                     .setFooter("un utilisateur est passé au niveau supérieur ")
                     .setTimestamp()
                 await Root.channels.cache.get("781624931787472977").send({ embed: RankUpdate })
+                
+                if (dbUser.level = 5 && message.member.roles.cache.find(r => r.id = "747459650282323995")) {
+                    message.member.roles.remove("747459650282323995");
+                    message.member.roles.add("747459650286387230");
+                }
+
+                if (dbUser.level = 10 && message.member.roles.cache.find(r => r.id = "747459650286387230")) {
+                    message.member.roles.remove("747459650286387230");
+                    message.member.roles.add("747459650282323996");
+                }
+
+                if (dbUser.level = 15 && message.member.roles.cache.find(r => r.id = "747459650282323996")) {
+                    message.member.roles.remove("747459650282323996");
+                    message.member.roles.add("747459650282323997");
+                }
+
+                if (dbUser.level = 20 && message.member.roles.cache.find(r => r.id = "747459650282323997")) {
+                    message.member.roles.remove("747459650282323997");
+                    message.member.roles.add("747459650286387231");
+                }
+
+                if (dbUser.level = 25 && message.member.roles.cache.find(r => r.id = "747459650286387231")) {
+                    message.member.roles.remove("747459650286387231");
+                    message.member.roles.add("747459650286387232");
+                }
+
+                if (dbUser.level = 30 && message.member.roles.cache.find(r => r.id = "747459650286387232")) {
+                    message.member.roles.remove("747459650286387232");
+                    message.member.roles.add("747459650286387234");
+                }
+
+                if (dbUser.level = 35 && message.member.roles.cache.find(r => r.id = "747459650286387234")) {
+                    message.member.roles.remove("747459650286387234");
+                    message.member.roles.add("747459650286387233");
+                }
+
+                if (dbUser.level = 40 && message.member.roles.cache.find(r => r.id = "747459650286387233")) {
+                    message.member.roles.remove("747459650286387233");
+                    message.member.roles.add("747459650286387235");
+                }
+
+                if (dbUser.level = 45 && message.member.roles.cache.find(r => r.id = "747459650286387235")) {
+                    message.member.roles.remove("747459650286387235");
+                    message.member.roles.add("747459650286387236");
+                }
+
+                if (dbUser.level = 50 && message.member.roles.cache.find(r => r.id = "747459650286387236")) {
+                    message.member.roles.remove("747459650286387236");
+                    message.member.roles.add("747459650286387237");
+                }
+
+                if (dbUser.level = 55 && message.member.roles.cache.find(r => r.id = "747459650286387237")) {
+                    message.member.roles.remove("747459650286387237");
+                    message.member.roles.add("747459650286387238");
+                }
+
+                if (dbUser.level = 60 && message.member.roles.cache.find(r => r.id = "747459650286387238")) {
+                    message.member.roles.remove("747459650286387238");
+                    message.member.roles.add("747459650286387239");
+                }
 
             }
 
