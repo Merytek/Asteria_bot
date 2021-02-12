fe5e421678f99c694504f943c1c39d4aebcdc449
Aether <asp3kt47@gmail.com>
2021-02-12 16:55:29 +0100

    Ajout message de départ

--- event/member/guildMemberRemove.js
+++ event/member/guildMemberRemove.js
@@ -1,3 +1,14 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = (Root, member) => {
+    const ByeEmbed = new MessageEmbed()
     
+        .setColor('#dc143c')
+        .setTitle(`__${member.user.username} a quitté le serveur__`)
+        .setDescription(`disons au revoir à cette jeune personne partie trop tôt 😭`)
+        .setFooter("un utilisateur a quitté le serveur")
+        .setTimestamp()
+    
+    Root.channels.cache.get("747459650890498060").send({ embed: ByeEmbed });
+
 }
