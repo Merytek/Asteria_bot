726b090920c1c0db419527c762c7e53ceffcd733
Merytek <asp3kt47@gmail.com>
2021-03-26 22:04:35 +0100

    mise en place de la seconde partie de l'handling des alias

--- event/client/message.js
+++ event/client/message.js
@@ -1,61 +1,85 @@
 const { PREFIX } = require("../../config.json");
+const { error } = require("console");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = async (Root, message) => {
-    const dbUser = await Root.getUser(message.member);
 
-    if (message.channel.type === "dm") {
-        return Root.emit("directMessage", message);
-    }
+    if (!message.author.bot) {
+        const dbUser = await Root.getUser(message.member);
+        //const CharUser = await Root.getCharacter(message.member);
 
-    if (message.author.bot) return;
+        if (message.content.startsWith(PREFIX) && !message.author.bot) {
+            const args = message.content.slice(PREFIX.length).split(/ +/);
+            const commandName = args.shift().toLowerCase();
 
-    if (!dbUser && !message.author.bot) {
-        await Root.createUser({
-            guildID: message.member.guild.id,
-            guildName: message.member.guild.name,
-            userID: message.member.id,
-            username: message.member.user.tag,
-        });
-    }
+            const command = Root.commands.get(commandName) || Root.commands.find(cmd => cmd.help.alias && cmd.help.alias.includes(commandName));
+            command.run(Root, message, args, dbUser);
+        }
 
-    if (dbUser.experience >= dbUser.requis) {
-                
-        XpRest = dbUser.experience - dbUser.requis;
+        const BannedDiscordLink = new RegExp(/https(:)\/\/discord.gg\/[a-zA-Z0-9]+/g);
+        const BannedYoutubeLink = new RegExp(/https(:)\/\/www.youtube.com\/watch\?v=[a-zA-Z0-9]+/g);
 
-        Root.updateLevel(Root, message.member, XpRest);
+        if (message.channel.type === "dm") {
+            return Root.emit("directMessage", message);
+        }
 
-        const RankUpdate = new MessageEmbed()
+        if (!dbUser) {
+            await Root.createUser({
+                guildID: message.member.guild.id,
+                guildName: message.member.guild.name,
+                userID: message.member.id,
+                username: message.member.user.tag,
+            });
+        } else {
 
-            .setColor("#DAF450")
-            .setTitle(`${message.member.user.username} est passé au niveau supérieur`)
-            .setDescription(`bravo tu es passé au niveau ${dbUser.level + 1}`)
-            .setThumbnail(message.member.user.displayAvatarURL())
-            .setFooter("un utilisateur est passé au niveau supérieur ")
-            .setTimestamp()
-        Root.channels.cache.get("781624931787472977").send({ embed: RankUpdate })
+            if (dbUser) {
 
-    }
+                const Condition = Math.floor((Math.random() * 49) + 1);
+                if (Condition < 30 && Condition > 25) {
+                    const max = 20
+                    const min = 5
+                    let Xpgain = Math.floor((Math.random() * max) + min);
 
-    if (dbUser && !message.author.bot) {
-        const Condition = Math.floor((Math.random() * 49) + 1);
-        if (Condition < 30 && Condition > 25) {
-            const max = 20
-            const min = 5
-            let Xpgain = Math.floor((Math.random() * max) + min);
+                    Root.updateXp(Root, message.member, Xpgain);
 
-            Root.updateXp(Root, message.member, Xpgain);
+                }
 
+                if (dbUser.experience >= dbUser.requis) {
+
+                    const RankUpdate = new MessageEmbed()
+
+                        .setColor("#DAF450")
+                        .setTitle(`${message.member.user.username} est passé au niveau supérieur`)
+                        .setDescription(`bravo tu es passé au niveau ${dbUser.level + 1}`)
+                        .setThumbnail(message.member.user.displayAvatarURL())
+                        .setFooter("un utilisateur est passé au niveau supérieur ")
+                        .setTimestamp()
+                    Root.channels.cache.get("781624931787472977").send(RankUpdate).catch(error);
+
+
+                    XpRest = dbUser.experience - dbUser.requis;
+                    await Root.updateLevel(Root, message.member, XpRest);
+
+                    if (dbUser.level < 65) {
+                        if (dbUser.level.toString().endsWith("5") || dbUser.level.toString().endsWith("0")) {
+                            //dernière couche de vérification + ajout du nouveau rôle et suppression de l'ancien
+                        } else return;
+                    }
+                }
+            }
+        }
+
+        if (message.type === "USER_PREMIUM_GUILD_SUBSCRIPTION") {
+            const ServerBooster = new MessageEmbed()
+
+                .setColor("#8A1200")
+                .setTitle(`**${message.author.username}** vient de booster le serveur ! ❤`)
+                .setImage("https://media.tenor.co/videos/7c405f4646bbe6def9fc888080263d2a/mp4")
+                .setThumbnail("https://media.tenor.co/videos/311f027c8ed6b6a33e8c36fe65e2ecc8/mp4")
+                .setTimestamp()
+                .setFooter("un utilisateur a boosté le serveur")
+
+            Root.channel.cache.get("820865491018579989").send(ServerBooster).catch(error);
         }
     }
-
-
-    if (message.content.startsWith(PREFIX) && !message.author.bot) {
-        const args = message.content.slice(PREFIX.length).split(/ +/);
-        const command = args.shift().toLowerCase();
-
-        if (!Root.commands.has(command)) return;
-        Root.commands.get(command).run(Root, message, args, dbUser);
-    }
 }
-
--- event/client/ready.js
+++ event/client/ready.js
@@ -2,8 +2,8 @@ const { PREFIX } = require("../../config.json");
 
 module.exports = (Root) => {
     const activities = ["planter des arbres", 
-                        "chanter avec Wejdene", 
-                        "unifier la relativité générale et la mécanique quantique", 
+                        "chanter avec Wedjene", 
+                        "unifier la relativité général et la mécanique quantique", 
                         "soigner sa calvitie", 
                         "trainer sur youtube", 
                         "call of duty cold wars", 
@@ -11,7 +11,12 @@ module.exports = (Root) => {
                         "jongler avec des micro-ondes", 
                         "manger de la ferrite", 
                         "changer son alim", 
-                        "décharger OmégaMachina de ses électrons"]; 
+                        "décharger OmégaMachina de ses électrons",
+                        "préparer un plan pour dominer le monde",
+                        "faire des câlin à Nervous Nightingales",
+                        "rêver de faire le tour du monde",
+                        "programmer en Lisp",
+                        "Root.user.setActivity(`${activities[actID]}`);"]; //les divers statut du bot
 
     console.log(`connecté en tant que: ${Root.user.tag}`)
     Root.user.setStatus("dnd")
@@ -19,4 +24,5 @@ module.exports = (Root) => {
         let actID = Math.floor(Math.random() * Math.floor(activities.length));
         Root.user.setActivity(`${activities[actID]} [${PREFIX}help pour afficher l'aide]`);
     }, 60000)
+
 }
